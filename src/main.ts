53e940ea4fd5bc2ddea50882576cb18cb8c3dd1a
YILS <yils_lin@163.com>
2025-09-26 10:21:38 +0800

    修复vuetify/styles类型报错

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import 'vuetify/styles'
+import 'vuetify/styles/main.sass'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
